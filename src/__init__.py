1505dc8438e877c1700e493a8b3d21fb51ed6fca
Unmystic <unmystic7@gmail.com>
2025-08-08 12:08:50 +0200

    connect pg db

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from src.books.routes import router
+from contextlib import asynccontextmanager
+from src.db.main import initdb
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await initdb()
+    yield
+    print("Server is stopping")
+
 
 version = "v1"
 
@@ -7,6 +17,7 @@ app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
     version=version,
+    lifespan=lifespan,
 )
 
 app.include_router(router, prefix=f"/api/{version}/books", tags=["books"])
